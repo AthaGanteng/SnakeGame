8c7d43f91554ebbcd88b20b822fba2eb3c189f85
leo-rhamanata <167918703+GhostXvestia@users.noreply.github.com>
2026-05-26 14:49:58 +0800

    Role 3:Fix formatting in Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("  Memulai Snake Game...");
-
+        
         try {
             GameController controller = new GameController();
             controller.startGame();

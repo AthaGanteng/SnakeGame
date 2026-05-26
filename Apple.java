94b6c021c13ec24aceee5294f7caaad8e89a6dfd
AthaGanteng <clifdyle.gmail.com>
2026-05-26 13:54:55 +0700

    membuat class

---
+++ Apple.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+public class Apple extends Entity {
+    private static final char APPLE_SYMBOL = '@';
+    private Random random;
+
+    public Apple(int boardWidth, int boardHeight) {
+        super(0, 0, APPLE_SYMBOL);
+        this.random = new Random();
+        respawn(boardWidth, boardHeight);
+    }
+
+
+    public void respawn(int boardWidth, int boardHeight) {
+  
+        this.x = 1 + random.nextInt(boardWidth - 2);
+        this.y = 1 + random.nextInt(boardHeight - 2);
+    }
+
+    @Override
+    public char render() {
+        return APPLE_SYMBOL;
+    }
+}
